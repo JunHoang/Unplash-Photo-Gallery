46acc564af823ec42ff2f5206bd9a303b198fcdf
DungHoang <kathymaidung@gmail.com>
2020-11-06 16:29:02 +0200

    HandleClick

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './assets/css/style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App({title}) {
+//     return (
+//         <div>
+//             <div className="bg-gray-600 text-white p-5 border" >{title}</div>
+//         </div>
+//     ) 
+        
+// }
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {title: "Hello React", isshowing: false};
+    }
+
+    handleClick = () => {
+        this.setState({isshowing: !this.state.isshowing})
+    }
+
+    render() {
+        return (
+            <section className="flex justify-center">
+                <div className="w-1/2">
+                <div className="text-center">
+                    <div className="my-4">{this.state.title}</div>
+                        <button className="p-1 bg-blue-700 text-white my-2"
+                            onClick={this.handleClick}>Toggle Image</button>
+                    </div>
+                    {
+                        this.state.isshowing ? 
+                        (<img src="https://images.unsplash.com/photo-1592419044706-39796d40f98c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1911&q=80" />
+                        ) : null
+                    }
+                </div>
+            </section>
+
+        );
+    }
+} 
 
 export default App;
