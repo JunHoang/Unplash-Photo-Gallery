19bedfc717892b43f1b2b2dd60271fbdd015c87a
DungHoang <kathymaidung@gmail.com>
2020-12-08 23:35:57 +0200

    LifeCycle -useEffect -Did Mount and UnMount

--- src/components/images.js
+++ src/components/images.js
@@ -1,32 +1,54 @@
-import React, { Component } from 'react';
+import React, { useEffect } from "react";
 
-export class Images extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {interval: null};
-    }
+export default function Images() {
+  useEffect(() => {
+    //Did Mount
+    console.log("Images Comp mounted");
+    const interval = setInterval(() => {
+      console.log("Hello");
+    }, 1000);
 
-    componentDidMount() {
-        console.log("Images Comp mounted");
-        this.setState({
-            interval: setInterval(() =>{
-                console.log("Hello");
-            },1000),
-        });
-    }
+    //Did Unmount
+    return () => {
+      console.log("Images Comp unmounted");
+      clearInterval(interval);
+    };
+  }, []);
 
-    componentWillUnmount() {
-        console.log("Images Comp unmounted");
-        clearInterval(this.state.interval);
-    }
-
-    render() {
-        return (
-            <div>
-                <img src="https://images.unsplash.com/photo-1592419044706-39796d40f98c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1911&q=80" />
-            </div>
-        )
-    }
+  return (
+    <div>
+      <img src="https://images.unsplash.com/photo-1592419044706-39796d40f98c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1911&q=80" />
+    </div>
+  );
 }
 
-export default Images;
+// export class Images extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { interval: null };
+//   }
+
+//   componentDidMount() {
+//     console.log("Images Comp mounted");
+//     this.setState({
+//       interval: setInterval(() => {
+//         console.log("Hello");
+//       }, 1000),
+//     });
+//   }
+
+//   componentWillUnmount() {
+//     console.log("Images Comp unmounted");
+//     clearInterval(this.state.interval);
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <img src="https://images.unsplash.com/photo-1592419044706-39796d40f98c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1911&q=80" />
+//       </div>
+//     );
+//   }
+// }
+
+//  export default Images;
