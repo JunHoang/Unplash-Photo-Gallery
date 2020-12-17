92aa0c27f05bb9c1b99286a88092b7c3217da424
DungHoang <kathymaidung@gmail.com>
2020-12-17 15:57:31 +0200

    Delete image

--- src/App.js
+++ src/App.js
@@ -1,92 +1,20 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState} from "react";
 import "./assets/css/style.css";
 import Images from "./components/images";
 
 function App() {
   const [title, setTitle] = useState("Hello React");
-  const [isShowing, setIsShowing] = useState(false);
-  const [didMount, setdidMount] = useState(false);
-
-  const mountRef = useRef(false);
-
-  //Component Did Mount only
-  useEffect(() => {
-      setdidMount(true);
-      console.log("App Mounted")
-  }, []);
-
-  //Component Will Update
-  useEffect(() =>{
-      if (mountRef.current) {
-          console.log("App Updated");
-      } else {
-          mountRef.current = true;
-      }
-  }, [isShowing])
-
-  function handleClick () {
-    setIsShowing(!isShowing);
-  };
 
   return (
     <section className="flex justify-center">
-        {console.log("re-render")}
-      <div className="w-1/2">
+      <div className="w-10/12">
         <div className="text-center">
           <div className="my-4">{title}</div>
-          <button
-            className="p-1 bg-blue-700 text-white my-2"
-            onClick={handleClick}
-          >
-            Toggle Image
-          </button>
+          <Images />
         </div>
-        {isShowing ? <Images /> : null}
       </div>
     </section>
   );
 }
-
-// class App extends React.Component {
-//   constructor(props) {
-//     console.log("App constructor");
-
-//     super(props);
-//     this.state = { title: "Hello React", isshowing: false };
-//   }
-
-//   componentDidMount() {
-//     console.log("App mounted");
-//     // this.setState({title: "Hello LifeCycle!"})
-//   }
-
-//   componentDidUpdate() {
-//     console.log("App updated");
-//   }
-
-//   handleClick = () => {
-//     this.setState({ isshowing: !this.state.isshowing });
-//   };
-
-//   render() {
-//     console.log("App Render");
-//     return (
-//       <section className="flex justify-center">
-//         <div className="w-1/2">
-//           <div className="text-center">
-//             <div className="my-4">{this.state.title}</div>
-//             <button
-//               className="p-1 bg-blue-700 text-white my-2"
-//               onClick={this.handleClick}
-//             >
-//               Toggle Image
-//             </button>
-//           </div>
-//           {this.state.isshowing ? <Images /> : null}
-//         </div>
-//       </section>
-//     );
-//   }
-// }
 
 export default App;
--- src/components/images.js
+++ src/components/images.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 
 export default function Images() {
-
   const [images, setimages] = useState([
     "https://images.unsplash.com/photo-1495562569060-2eec283d3391?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjZ8fG91dGRvb3J8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
     "https://images.unsplash.com/photo-1444492417251-9c84a5fa18e0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80",
@@ -10,25 +9,39 @@ export default function Images() {
     "https://images.unsplash.com/photo-1605539558089-0c14c173b015?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDE5OHw2c01WalRMU2tlUXx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
   ]);
 
-  const [newImageURL, setnewImageURL] = useState("")
+  const [newImageURL, setnewImageURL] = useState("");
 
   function handleAdd() {
-    setimages([
-      ...images,
-      newImageURL
-    ]);
-    setnewImageURL("");
+    if (newImageURL !== "") {
+      setimages([...images, newImageURL]);
+      setnewImageURL("");
+    }
   }
 
   function handleChange(event) {
     setnewImageURL(event.target.value);
   }
 
+  function handleRemove(index) {
+    // setimages(images.filter((image,i) => i !== index));
+
+    setimages([
+      ...images.slice(0, index),
+      ...images.slice(index + 1, images.slength),
+    ]);
+  }
+
   function ShowImage() {
-    return images.map((image) => {
+    return images.map((image, index) => {
       return (
-        <div className="w-1/3 my-1">
-          <img src={image} width="150"/>
+        <div className="w-1/3 my-1 flex justify-center" key={index}>
+          <div className="relative">
+            <i
+              className="fas fa-times absolute right-0 cursor-pointer opacity-25 hover:opacity-100"
+              onClick={() => handleRemove(index)}
+            ></i>
+            <img src={image} width="150" />
+          </div>
         </div>
       );
     });
@@ -40,12 +53,25 @@ export default function Images() {
         <ShowImage />
       </div>
       <div className="flex justify-between">
-        <input 
-          type="text" 
-          className="p-1 my-5 border border-gray-800 shadow rounded" 
-          onChange={handleChange} 
-          value={newImageURL}/>
-        <button className="p-1 my-5 bg-green-600 text-white" onClick={handleAdd}>Add New</button>
+        <div className="w-full">
+          <input
+            type="text"
+            className="p-1 my-5 border border-gray-800 shadow rounded w-full"
+            onChange={handleChange}
+            value={newImageURL}
+          />
+        </div>
+
+        <div className="">
+          <button
+            disabled={newImageURL === ""}
+            className={`p-1 my-5 ml-5 text-white
+          ${newImageURL !== "" ? "bg-green-600" : "bg-green-300"}`}
+            onClick={handleAdd}
+          >
+            Add
+          </button>
+        </div>
       </div>
     </section>
   );
