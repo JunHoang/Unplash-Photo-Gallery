62e814b3327e7892aab1a6efc0ed07f7cf6aa869
DungHoang <kathymaidung@gmail.com>
2020-12-19 14:17:52 +0200

    axios get

--- src/components/images.js
+++ src/components/images.js
@@ -1,37 +1,22 @@
+import Axios from "axios";
 import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import Image from "./image";
 
 export default function Images() {
-  const [images, setimages] = useState([
-    "https://images.unsplash.com/photo-1608226037285-c9f7da4cf3e2?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3OXx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60",
-    "https://images.unsplash.com/photo-1444492417251-9c84a5fa18e0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80",
-    "https://images.unsplash.com/photo-1605539558089-0c14c173b015?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDE5OHw2c01WalRMU2tlUXx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    "https://images.unsplash.com/photo-1502394202744-021cfbb17454?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzF8fG91dGRvb3J8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    "https://images.unsplash.com/photo-1501960645408-12c9bcfca49f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NTB8fG91dGRvb3J8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-  ]);
+  const [images, setimages] = useState([]);
 
   const inputRef = useRef(null);
-  const varRef = useRef(images.length);
   const [newImageURL, setnewImageURL] = useState("");
-  const [myName, setmyName] = useState("Dung")
 
   useEffect(() => {
     inputRef.current.focus();
-    console.log(varRef);
-  }, [])
+    Axios.get(
+      "https://api.unsplash.com/photos/?client_id=wamrH9s9tvasRKNiL56EIJ_0K6Dmo2nVVm_GyIQGiyQ"
+    ).then((res) => {
+      setimages(res.data);
+    });
+  }, []);
 
-  useEffect(() => {
-    varRef.current = varRef.current + 1;
-  })
-
-  useEffect(() => {
-    console.log("this is useEffect");
-  })
-
-  useLayoutEffect(() => {
-    setmyName("ReactJS")
-    console.log("this is useLayoutEffect")
-  })
 
   function handleAdd() {
     if (newImageURL !== "") {
@@ -55,14 +40,17 @@ export default function Images() {
 
   function ShowImage() {
     return images.map((img, index) => (
-      <Image image={img} handleRemove={handleRemove} index={index} key={index} />
+      <Image
+        image={img.urls.regular}
+        handleRemove={handleRemove}
+        index={index}
+        key={index}
+      />
     ));
   }
 
   return (
     <section>
-      <p>My name is {myName}</p>
-      <p>Component is updated {varRef.current} times</p>
       <div className="flex flex-wrap justify-center">
         <ShowImage />
       </div>
