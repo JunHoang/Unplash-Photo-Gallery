d199aaedf97da62116a03b7d822698426a27597a
DungHoang <kathymaidung@gmail.com>
2021-01-08 11:05:04 +0200

    Add Loading for whole page

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -4,7 +4,7 @@ export default function Loading() {
     return (
         <div className="flex h-screen">
         <p className="m-auto">
-          <i className="fa fa-circle-notch fa-spin text-5xl text-blue-400"></i>
+          <i className="fa fa-circle-notch fa-spin text-5xl text-blue-600"></i>
         </p>
       </div>
     )
