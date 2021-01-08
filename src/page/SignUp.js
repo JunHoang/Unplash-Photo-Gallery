a33834c06a341f9dc27c11e6c9cbc2af296ab9a6
DungHoang <kathymaidung@gmail.com>
2021-01-08 19:38:56 +0200

    Leveraging React Conext using Formik

--- src/page/SignUp.js
+++ src/page/SignUp.js
@@ -1,4 +1,4 @@
-import { Formik, useFormik } from "formik";
+import { ErrorMessage, Field, Form, Formik, useFormik } from "formik";
 import React from "react";
 import * as Yup from "yup";
 
@@ -20,48 +20,42 @@ export default function SignUp() {
           .min(6, "Password must be longer than 6 characters."),
       })}
     >
-      {(formik) => (
-        <div className="flex h-screen bg-gray-200">
-          <div className="m-auto w-1/3 text-white flex flex-wrap justify-center shadow-lg rounded-lg bg-gradient-to-br from-indigo-900 to-indigo-700">
-            <form className="m-5 w-10/12" onSubmit={formik.handleSubmit}>
-              <h1 className="w-full text-4xl tracking-widest text-center my-6">
-                Sign Up Here
-              </h1>
-              <div className="w-full my-6">
-                <input
-                  type="email"
-                  className="p-2 rounded shadow w-full text-black"
-                  placeholder="Email or Username"
-                  {...formik.getFieldProps("email")}
-                />
-                {formik.touched.email && formik.errors.email ? (
-                  <p>{formik.errors.email}</p>
-                ) : null}
-              </div>
-              <div className="w-full my-6">
-                <input
-                  type="password"
-                  className="p-2 rounded shadow w-full text-black"
-                  placeholder="Password"
-                  {...formik.getFieldProps("password")}
-                />
-                {formik.touched.password && formik.errors.password ? (
-                  <p>{formik.errors.password}</p>
-                ) : null}
-              </div>
-              <div className="w-full my-10">
-                <button
-                  type="submit"
-                  className="p-2 rounded shadow w-full bg-gradient-to-tr from-yellow-600 to-yellow-400 text-black"
-                >
-                  {/* {isLoading ? (<i className="fas fa-circle-notch fa-spin" />): ("SignUp")} */}
-                  SignUp
-                </button>
-              </div>
-            </form>
-          </div>
+      <div className="flex h-screen bg-gray-200">
+        <div className="m-auto w-1/3 text-white flex flex-wrap justify-center shadow-lg rounded-lg bg-gradient-to-br from-indigo-900 to-indigo-700">
+          <Form className="m-5 w-10/12">
+            <h1 className="w-full text-4xl tracking-widest text-center my-6">
+              Sign Up Here
+            </h1>
+            <div className="w-full my-6">
+              <Field
+                name="email"
+                type="email"
+                className="p-2 rounded shadow w-full text-black"
+                placeholder="Email or Username"
+              />
+              <ErrorMessage name="email" />
+            </div>
+            <div className="w-full my-6">
+              <Field
+                name="password"
+                type="password"
+                className="p-2 rounded shadow w-full text-black"
+                placeholder="Password"
+              />
+              <ErrorMessage name="password" />
+            </div>
+            <div className="w-full my-10">
+              <button
+                type="submit"
+                className="p-2 rounded shadow w-full bg-gradient-to-tr from-yellow-600 to-yellow-400 text-black"
+              >
+                {/* {isLoading ? (<i className="fas fa-circle-notch fa-spin" />): ("SignUp")} */}
+                SignUp
+              </button>
+            </div>
+          </Form>
         </div>
-      )}
+      </div>
     </Formik>
   );
 }
